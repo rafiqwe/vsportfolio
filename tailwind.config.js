6761349331fbf073846be4a0dbbc7c5e637c9602
rafiqwe <rmlmanik502@gmail.com>
2026-01-25 21:45:17 +0600

    Created: tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", "sans-serif"],
+        mono: ["var(--font-jet-mono)", "monospace"],
+      },
+    },
+  },
+};
