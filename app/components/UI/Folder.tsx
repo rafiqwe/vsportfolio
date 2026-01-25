569278c8b6c580801f781670488eaff31cf03faa
rafiqwe <rmlmanik502@gmail.com>
2026-01-25 21:47:57 +0600

    Created: folder component

---
+++ app/components/UI/Folder.tsx
@@ -0,0 +1,57 @@
+"use client";
+import useTaps from "@/app/hook/use-Taps";
+import Link from "next/link";
+import React from "react";
+import { FaFolder, FaFolderOpen, FaReact } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
+
+const Folder = ({
+  name,
+  link,
+  from,
+}: {
+  name?: string;
+  link: string;
+  from?: string;
+}) => {
+  const [openFolders, setOpenFolders] = React.useState<boolean>(false);
+  const { addTap } = useTaps();
+
+  return (
+    <div className="w-full h-full">
+      {from === "project" ? "" : <h1 className="text-sm font-bold">{name}</h1>}
+      <div
+        onClick={() => setOpenFolders(!openFolders)}
+        className="flex items-center gap-2 py-1 rounded hover:bg-white/5 cursor-pointer border-l border-[var(--border-color)] h-full"
+      >
+        <div>
+          <IoIosArrowDown
+            className={`transition-transform duration-200 ${
+              !openFolders ? "-rotate-90" : ""
+            }`}
+          />
+        </div>
+        <div className="flex items-center gap-2">
+          {openFolders ? (
+            <FaFolderOpen size={15} className="text-blue-400" />
+          ) : (
+            <FaFolder size={15} className="text-blue-400" />
+          )}
+          <p className="text-sm">{name}</p>
+        </div>
+      </div>
+      {openFolders && (
+        <div className="pl-3" onClick={() => addTap(name!, link)}>
+          <Link href={link} className="flex items-center gap-2">
+            <p className="border-l pl-6 border-[var(--border-color)]">
+              <FaReact size={15} color="#008BFF" />
+            </p>
+            <p className="text-sm">page.tsx</p>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Folder;
