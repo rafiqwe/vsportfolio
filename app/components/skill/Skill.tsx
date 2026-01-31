c657637d542c37b5eea297c4d9c16162fc81b65a
rafiqwe <rmlmanik502@gmail.com>
2026-01-31 23:40:28 +0600

    Created: skill component

---
+++ app/components/skill/Skill.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import { FaFolder, FaFolderOpen, FaReact } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
+import Folder from "../UI/Folder";
+
+const Skills = () => {
+  return (
+    <div className="w-full h-full">
+      <Folder name="Skills" link="/skills" />
+    </div>
+  );
+};
+
+export default Skills;
