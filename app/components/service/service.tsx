384834556962fe72aa555d621dbbe573222ced24
rafiqwe <rmlmanik502@gmail.com>
2026-01-31 23:40:52 +0600

    Created: service component

---
+++ app/components/service/service.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Folder from '../UI/Folder'
+
+const Service = () => {
+  return (
+    <div className='w-full h-full'>
+       <Folder name='Services' link='/service' />
+    </div>
+  )
+}
+
+export default Service
