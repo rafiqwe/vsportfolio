7ca82eaf79045603c2a987eae2983c17cf3e6d85
rafiqwe <rmlmanik502@gmail.com>
2026-01-31 23:42:58 +0600

    Created: Contact component

---
+++ app/components/contact/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import Folder from "../UI/Folder";
+
+const Contact = () => {
+  return (
+    <div className="w-full h-full">
+      <Folder name="Contact" link="/contact" />
+    </div>
+  );
+};
+
+export default Contact;
