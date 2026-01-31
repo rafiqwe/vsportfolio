42dbd1fea012e14f5d2d16e5c2453b368b490d63
rafiqwe <rmlmanik502@gmail.com>
2026-01-31 23:42:09 +0600

    Created: About component

---
+++ app/components/about/About.tsx
@@ -0,0 +1,11 @@
+import Folder from "../UI/Folder";
+
+const About = () => {
+  return (
+    <div className="w-full h-full">
+      <Folder name="About" link="about" />
+    </div>
+  );
+};
+
+export default About;
