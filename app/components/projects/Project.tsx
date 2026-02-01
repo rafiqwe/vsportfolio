62a95467f903eddb1b17aca45bcc819dc94d7b0b
rafiqwe <rmlmanik502@gmail.com>
2026-02-01 23:45:58 +0600

    Created: Project component

--- app/components/projects/Project.tsx
+++ app/components/projects/Project.tsx
@@ -7,19 +7,28 @@ const Project = () => {
       link: "/project/devguide",
     },
     {
-      name: "Projects1",
-      link: "/projects",
+      name: "GenAi",
+      link: "/project/genai",
     },
     {
-      name: "Projects2",
-      link: "/projects",
+      name: "bgift-social",
+      link: "/project/bgift-social",
+    },
+    {
+      name: "vs-portfolio",
+      link: "/project/vs-portfolio",
     },
   ];
   return (
     <div>
       <h1 className="text-sm font-bold">Projects</h1>
       {projects.map((project, index) => (
-        <Folder key={index} link={project.link} name={project.name} from='project' />
+        <Folder
+          key={index}
+          link={project.link}
+          name={project.name}
+          from="project"
+        />
       ))}
     </div>
   );
