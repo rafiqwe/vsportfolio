2e1c0d647815359e90716093a3991dca744114b7
rafiqwe <rmlmanik502@gmail.com>
2026-02-01 23:43:55 +0600

    Created: Side list component

---
+++ app/components/layouts/SideList.tsx
@@ -0,0 +1,45 @@
+import {
+  Blocks,
+  BugOff,
+  CircleAlert,
+  Files,
+  FileUser,
+  Router,
+} from "lucide-react";
+import { MdContactPage } from "react-icons/md";
+
+interface SideListProps {
+  setTap: (tap: string) => void;
+  handleTapWeight: (tap: string) => void;
+  Tap: string;
+}
+
+const SideList = ({ setTap, handleTapWeight, Tap }: SideListProps) => {
+  const list = [
+    { icon: <Files />, name: "file" },
+    { icon: <MdContactPage size={25} />, name: "contact" },
+    { icon: <BugOff />, name: "skills" },
+    { icon: <Blocks />, name: "projects" },
+    { icon: <Router />, name: "service" },
+    { icon: <CircleAlert />, name: "about" },
+  ];
+  return (
+    <div>
+      <ul className="flex flex-col gap-1 space-y-4">
+        {list.map((item, index) => (
+          <li
+            onClick={() => {
+              handleTapWeight(item.name);
+            }}
+            className={`${item.name === "about" ? "rotate-180" : ""}  ${Tap === item.name ? "bg-[var(--tabs-bg)]" : ""} cursor-pointer hover:bg-[var(--tabs-bg)] text-[#008BFF]  p-2 rounded-md`}
+            key={index}
+          >
+            {item.icon}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SideList;
