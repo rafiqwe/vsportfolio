fef64b044dee0b3accdc67a4019a47696238bd68
rafiqwe <rmlmanik502@gmail.com>
2026-02-10 21:34:21 +0600

    Created: Tabs layout component

---
+++ app/components/layouts/Tabs.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Tabs() {
+  return (
+    <div className="w-full h-8 bg-[var(--tabs-bg)] border-b border-[var(--border-color)] overflow-x-auto flex items-center justify-center font-sans">
+      <div className="bg-[var(--editor-bg)] md:px-60 px-20 py-0.5 rounded-3xl  font-bold text-sm">
+          Muhammad Rabbi
+      </div>
+    </div>
+  );
+}
